cea711678eecc72207cc386090dea9ee8149f372
René Slijkoord <reneslijkoord@live.nl>
2023-08-07 23:04:28 +0200

    Started update for 1.20, not finished/tested yet!!

--- bukkit/src/main/java/com/github/dig/endervaults/bukkit/EVBukkitPlugin.java
+++ bukkit/src/main/java/com/github/dig/endervaults/bukkit/EVBukkitPlugin.java
@@ -38,6 +38,7 @@ import com.github.dig.endervaults.nms.v1_14_R1.v1_14_R1NMS;
 import com.github.dig.endervaults.nms.v1_17_R1.v1_17_R1NMS;
 import com.github.dig.endervaults.nms.v1_18_R1.*;
 import com.github.dig.endervaults.nms.v1_19_R2.*;
+import com.github.dig.endervaults.nms.v1_20_R1.*;
 import com.github.dig.endervaults.nms.v1_8_R3.v1_8_R3NMS;
 import lombok.extern.java.Log;
 import org.bukkit.Bukkit;
@@ -201,6 +202,9 @@ public class EVBukkitPlugin extends JavaPlugin implements EnderVaultsPlugin {
             case v1_19_R2:
             case v1_19_R3:
                 bridge = new v1_19_R2NMS();
+                break;
+            case V1_20_R1:
+                bridge = new V1_20_R1NMS();
                 break;
             default:
                 throw new InvalidMinecraftVersionException("Minecraft " + version.name() + " not supported.");
