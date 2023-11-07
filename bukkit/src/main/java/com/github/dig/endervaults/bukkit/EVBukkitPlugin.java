5be3af90e7f4ab9ad0ad36479cc4d7983cff31fd
Rubenicos <44579213+Rubenicos@users.noreply.github.com>
2023-11-07 17:33:39 -0600

    Update to 1.20

--- bukkit/src/main/java/com/github/dig/endervaults/bukkit/EVBukkitPlugin.java
+++ bukkit/src/main/java/com/github/dig/endervaults/bukkit/EVBukkitPlugin.java
@@ -200,6 +200,7 @@ public class EVBukkitPlugin extends JavaPlugin implements EnderVaultsPlugin {
                 break;
             case v1_19_R2:
             case v1_19_R3:
+            case v1_20_R1:
                 bridge = new v1_19_R2NMS();
                 break;
             default:
